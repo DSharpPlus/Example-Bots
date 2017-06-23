025303a20f3a52cb45de50d468c39f4f8e4bb4fe
Emzi0767 <matiz100@windowslive.com>
2017-06-23 20:54:20 +0200

    Updated voice example.

--- DSPlus.Examples.CSharp.Ex04/ExampleVoiceCommands.cs
+++ DSPlus.Examples.CSharp.Ex04/ExampleVoiceCommands.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Text;
 using System.Threading.Tasks;
 using DSharpPlus;
 using DSharpPlus.CommandsNext;
