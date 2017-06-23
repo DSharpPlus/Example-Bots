613f551f65ccfa086727942decd6b5a5b443e5db
Emzi0767 <matiz100@windowslive.com>
2017-06-23 20:54:07 +0200

    Added Interactivity and voice examples.

---
+++ DSPlus.Examples.CSharp.Ex04/ExampleVoiceCommands.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.VoiceNext;
+
+namespace DSPlus.Examples
+{
+    public class ExampleVoiceCommands
+    {
+        [Command("join"), Description("Joins a voice channel.")]
+        public async Task Join(CommandContext ctx, DiscordChannel chn = null)
+        {
+            // check whether VNext is enabled
+            var vnext = ctx.Client.GetVoiceNextClient();
+            if (vnext == null)
+            {
+                // not enabled
+                await ctx.RespondAsync("VNext is not enabled or configured.");
+                return;
+            }
+
+            // check whether we aren't already connected
+            var vnc = vnext.GetConnection(ctx.Guild);
+            if (vnc != null)
+            {
+                // already connected
+                await ctx.RespondAsync("Already connected in this guild.");
+                return;
+            }
+
+            // get member's voice state
+            var vstat = ctx.Member?.VoiceState;
+            if ((vstat == null || vstat.Channel == null) && chn == null)
+            {
+                // they did not specify a channel and are not in one
+                await ctx.RespondAsync("You are not in a voice channel.");
+                return;
+            }
+
+            // channel not specified, use user's
+            if (chn == null)
+                chn = vstat.Channel;
+
+            // connect
+            vnc = await vnext.ConnectAsync(chn);
+
+            await ctx.RespondAsync($"Connected to `{chn.Name}`");
+        }
+
+        [Command("leave"), Description("Leaves a voice channel.")]
+        public async Task Leave(CommandContext ctx)
+        {
+            // check whether VNext is enabled
+            var vnext = ctx.Client.GetVoiceNextClient();
+            if (vnext == null)
+            {
+                // not enabled
+                await ctx.RespondAsync("VNext is not enabled or configured.");
+                return;
+            }
+
+            // check whether we aren't already connected
+            var vnc = vnext.GetConnection(ctx.Guild);
+            if (vnc == null)
+            {
+                // already connected
+                await ctx.RespondAsync("Not connected in this guild.");
+                return;
+            }
+
+            // disconnect
+            vnc.Disconnect();
+
+            await ctx.RespondAsync($"Disconnected");
+        }
+
+        [Command("play"), Description("Plays an audio file.")]
+        public async Task Play(CommandContext ctx, [RemainingText, Description("Full path to the file to play.")] string filename)
+        {
+            // check whether VNext is enabled
+            var vnext = ctx.Client.GetVoiceNextClient();
+            if (vnext == null)
+            {
+                // not enabled
+                await ctx.RespondAsync("VNext is not enabled or configured.");
+                return;
+            }
+
+            // check whether we aren't already connected
+            var vnc = vnext.GetConnection(ctx.Guild);
+            if (vnc == null)
+            {
+                // already connected
+                await ctx.RespondAsync("Not connected in this guild.");
+                return;
+            }
+
+            if (!File.Exists(filename))
+            {
+                await ctx.RespondAsync($"File `{filename}` does not exist.");
+                return;
+            }
+
+            // wait for current playback to finish
+            while (vnc.IsPlaying)
+                await vnc.WaitForPlaybackFinishAsync();
+
+            Exception exc = null;
+            await ctx.Message.RespondAsync($"Playing `{filename}`");
+            await vnc.SendSpeakingAsync(true);
+            try
+            {
+                // borrowed from
+                // https://github.com/RogueException/Discord.Net/blob/5ade1e387bb8ea808a9d858328e2d3db23fe0663/docs/guides/voice/samples/audio_create_ffmpeg.cs
+
+                var ffmpeg_inf = new ProcessStartInfo
+                {
+                    FileName = "ffmpeg",
+                    Arguments = $"-i \"{filename}\" -ac 2 -f s16le -ar 48000 pipe:1",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true
+                };
+                var ffmpeg = Process.Start(ffmpeg_inf);
+                var ffout = ffmpeg.StandardOutput.BaseStream;
+
+                // lets buffer ffmpeg output
+                using (var ms = new MemoryStream())
+                {
+                    await ffout.CopyToAsync(ms);
+                    ms.Position = 0;
+
+                    var buff = new byte[3840]; // buffer to hold the PCM data
+                    var br = 0;
+                    while ((br = ms.Read(buff, 0, buff.Length)) > 0)
+                    {
+                        if (br < buff.Length) // it's possible we got less than expected, let's null the remaining part of the buffer
+                            for (var i = br; i < buff.Length; i++)
+                                buff[i] = 0;
+
+                        await vnc.SendAsync(buff, 20); // we're sending 20ms of data
+                    }
+                }
+            }
+            catch (Exception ex) { exc = ex; }
+            finally
+            {
+                await vnc.SendSpeakingAsync(false);
+            }
+
+            if (exc != null)
+                await ctx.RespondAsync($"An exception occured during playback: `{exc.GetType()}: {exc.Message}`");
+        }
+    }
+}
