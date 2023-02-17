704a28d6872a0abc5dfa4ff2225ccea6d82c423c
Lunar Starstrum <lunar@forsaken-borders.net>
2023-02-16 23:37:26 -0600

    Hello world

---
+++ src/Core/HelloWorld/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus.Entities;
+
+namespace DSharpPlus.ExampleBots.Core.HelloWorld
+{
+    // We're sealing it because nothing will be inheriting this class
+    public sealed class Program
+    {
+        // Remember to make your main method async! You no longer need to have both a Main and MainAsync method in the same class.
+        public static async Task Main()
+        {
+            // For the sake of examples, we're going to load our Discord token from an environment variable.
+            string? token = Environment.GetEnvironmentVariable("DISCORD_TOKEN");
+            if (string.IsNullOrWhiteSpace(token))
+            {
+                Console.WriteLine("Please specify a token in the DISCORD_TOKEN environment variable.");
+                Environment.Exit(1);
+
+                // For the compiler's nullability, unreachable code.
+                return;
+            }
+
+            // Next, we instantiate our client.
+            DiscordConfiguration config = new()
+            {
+                Token = token,
+
+                // We're asking for unprivileged intents, which means we won't receive any member or presence updates.
+                // Privileged intents must be enabled in the Discord Developer Portal.
+
+                // TODO: Enable the message content intent in the Discord Developer Portal.
+                // The bot will not function otherwise.
+                Intents = DiscordIntents.AllUnprivileged | DiscordIntents.MessageContents
+            };
+            DiscordClient client = new(config);
+
+            // Register a simple ping command
+            client.MessageCreated += async (client, eventArgs) =>
+            {
+                // We use StringComparison.InvariantCultureIgnoreCase for case-insensitive matching.
+                if (eventArgs.Message.Content.Equals("!ping", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    // Respond to the message with "Pong!"
+                    await eventArgs.Message.RespondAsync($"Pong! The gateway latency is {client.Ping}ms.");
+                }
+            };
+
+            // We can specify a status for our bot. Let's set it to "online" and set the activity to "with fire".
+            DiscordActivity status = new("with fire", ActivityType.Playing);
+
+            // Now we connect and log in.
+            await client.ConnectAsync(status, UserStatus.Online);
+
+            // And now we wait infinitely so that our bot actually stays connected.
+            await Task.Delay(-1);
+        }
+    }
+}
