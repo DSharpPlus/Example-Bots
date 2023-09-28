9acc8ea1ae33037d9def17e21173e1c56368e298
Lunar Starstrum <lunar@forsaken-borders.net>
2023-09-27 21:17:28 -0500

    Add example logs and update status in the logging example

--- src/DSharpPlus.Examples.Basics.Logging/Program.cs
+++ src/DSharpPlus.Examples.Basics.Logging/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using DSharpPlus.Entities;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Serilog;
@@ -25,7 +26,7 @@ namespace DSharpPlus.Examples.Basics.Logging
             // TODO: Add custom output format
             services.AddLogging(builder => builder.AddSerilog(new LoggerConfiguration()
                 .WriteTo.Console()
-                .WriteTo.File("logs/DSharpPlus.Examples.Basics.Logging.log", rollingInterval: RollingInterval.Day)
+                .WriteTo.File("logs/DSharpPlus.Examples.Basics.Logging..log", rollingInterval: RollingInterval.Day)
                 .MinimumLevel.Override("DSharpPlus", LogEventLevel.Information) // DSharpPlus is very verbose, so we set it to only log Information and above
                 .MinimumLevel.Debug() // The rest of our application is less verbose, so we set it to Debug
                 .CreateLogger()));
@@ -43,8 +44,11 @@ namespace DSharpPlus.Examples.Basics.Logging
                 //LoggerFactory = NullLoggerFactory.Instance
             });
 
+            // Create an activity to display on Discord
+            DiscordActivity activity = new("some logs fall...", ActivityType.Watching);
+
             // Connect to the Discord gateway
-            await discord.ConnectAsync();
+            await discord.ConnectAsync(activity, UserStatus.Idle);
 
             ILogger<Program> logger = serviceProvider.GetRequiredService<ILogger<Program>>();
             logger.LogInformation("Discord bot started!");
